0f163ff5f4e353485e059a15de7e6ef03382f2e6
Paul Duchesne <45246635+paulduchesne@users.noreply.github.com>
2023-07-23 11:41:10 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,45 +1,47 @@
+# raspberry updates
 
-# pull dependancies
+sudo apt-get update  
+sudo apt-get -y upgrade  
+sudo apt-get dist-upgrade  
+sudo apt-get -y install libsamplerate0-dev libsndfile1-dev libasound2-dev libavahi-client-dev libreadline-dev libfftw3-dev libudev-dev cmake git 
 
-sudo apt-get -y install build-essential \
-  libsndfile1-dev \
-  libasound2-dev \
-  libavahi-client-dev \
-  libicu-dev \
-  libreadline6-dev \
-  libncurses5-dev \
-  libfftw3-dev \
-  libxt-dev \
-  libudev-dev \
-  pkg-config \
-  git \
-  cmake \
-  qtbase5-dev \
-  qt5-qmake \
-  qttools5-dev \
-  qttools5-dev-tools \
-  qtdeclarative5-dev \
-  qtpositioning5-dev \
-  libqt5sensors5-dev \
-  libqt5opengl5-dev \
-  qtwebengine5-dev \
-  libqt5svg5-dev \
-  libqt5websockets5-dev \
-  libjack-jackd2-dev 
+# install jack
 
-# pull it repo
+git clone https://github.com/jackaudio/jack2.git --depth 1 
+cd jack2  
+./waf configure --alsa --libdir=/usr/lib/arm-linux-gnueabihf/  
+./waf build  
+sudo ./waf install  
+sudo ldconfig  
+cd ..  
+rm -rf jack2  
+sudo sh -c "echo @audio - memlock 256000 >> /etc/security/limits.conf"  
+sudo sh -c "echo @audio - rtprio 75 >> /etc/security/limits.conf"  
+echo /usr/local/bin/jackd -P75 -dalsa -dhw:pisound -r48000 -p512 -n3 > ~/.jackdrc  
 
-git clone --recursive https://github.com/supercollider/supercollider.git
+# install supercollider
 
-# build commands
+git clone --recursive http://github.com/supercollider/supercollider
+cd supercollider  
+git checkout main 
+git submodule init && git submodule update  
+mkdir build && cd build  
+cmake -L -DCMAKE_BUILD_TYPE="Release" -DBUILD_TESTING=OFF -DSUPERNOVA=OFF -DNATIVE=ON -DSC_IDE=OFF -DNO_X11=ON -DSC_QT=OFF -DSC_ED=OFF -DSC_EL=OFF -DSC_VIM=ON ..  
+make  
+sudo make install  
+sudo ldconfig  
 
-cd supercollider/
-mkdir build 
-cd build
-cmake -DNATIVE=ON -DSC_EL=OFF ..
-make
-sudo make install
-make -j4
-sudo make install
-sudo ldconfig
+# autostart script
 
+cat >~/autostart.sh <<EOF
+#!/bin/bash
+export PATH=/usr/local/bin:$PATH
+export DISPLAY=:0.0
+sleep 10 # can be lower (5) for rpi3
+sclang ~/everything/test.scd
+EOF
+
+chmod +x ~/autostart.sh
+
+echo "@reboot ~/autostart.sh" >> ~/cron.txt
+crontab cron.txt
