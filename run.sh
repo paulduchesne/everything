3eac7ee0b6dec2bed8cd01e3843737c21755acae
Paul Duchesne <paulduchesne@tuta.io>
2025-01-01 23:12:48 +1100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sleep 2
+
+# required config.
+
+export PATH=$PATH:/usr/local/bin/
+export QT_QPA_PLATFORM=offscreen
+export JACK_NO_AUDIO_RESERVATION=1
+
 # run supercollider project.
 
-sclang ~/everything/everything.scd
+/usr/local/bin/sclang ~/everything/everything.scd
